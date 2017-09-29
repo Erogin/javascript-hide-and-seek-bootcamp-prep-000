ca65aa386bf39fd77e7b7c6212bbee21ff11b95e
Eric Roginek <eroginek@protonmail.com>
2017-09-29 15:42:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ function nestedTarget(){
   //pulls a .target out of #nested (# is used for IDs in selectors)
   const lis = document.getElementById('nested').querySelectorAll('div')
   for (let i = 0; i < lis.length; i++) {
-    if (lis[i].innerHTML === lis[i].target){
-      return lis[i].innerHTML
+    if (lis[i].innerHTML === lis.length - 1){
+      return lis[i]
     }
   }
   return lis
