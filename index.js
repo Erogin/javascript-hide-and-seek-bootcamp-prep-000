f8ab4babc72555f51d0a0112e42ad7c97ec1016c
Eric Roginek <eroginek@protonmail.com>
2017-09-29 15:02:51 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function getFirstSelector(selector){
+  //accepts a selector and returns the first element that matches
+  var docElements = []
+  var docElements = document.getElementsByTagName(`${selector}`)
+  return docElements[0]
+}
+
+function nestedTarget(){
+  //pulls a .target out of #nested (# is usd for IDs in selectors)
+}
+
+function increaseRankBy(n){
+  //increases the ranks in all of the .ranked-list s by n (use parseInt)
+}
+
+function deepestChild(){
+  //pulls out the most deeply nested child from div#grand-node.
+  //(Remember you can iterate over elements and call querySelector()
+  // and querySelectorAll() on them)
+}
